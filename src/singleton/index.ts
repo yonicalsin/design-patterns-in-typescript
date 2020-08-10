09f52fe3ea25c4306d7d268793fd60f09974c2ac
Yoni Calsin <yonicb20@gmail.com>
2020-08-10 11:22:11 -0500

    feat: 🎸 created the connection for single pattern

--- src/singleton/index.ts
+++ src/singleton/index.ts
@@ -1,1 +1,22 @@
-console.log('');
+class Connection {
+   private static instance: Connection;
+   private constructor() {}
+   public static getInstance() {
+      if (this.instance == null) {
+         this.instance = new Connection();
+      }
+      return this.instance;
+   }
+   public connect(): void {
+      console.log('Connected !');
+   }
+   public disconnect(): void {
+      console.log('Disconnect !');
+   }
+}
+
+const con = Connection.getInstance();
+con.connect();
+con.disconnect();
+
+console.log(con instanceof Connection); // true
